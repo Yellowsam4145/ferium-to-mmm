09a797b4a641270faa16a3c772704db083edf1ca
Yellowsam <69874104+Yellowsam4145@users.noreply.github.com>
2025-12-29 12:37:51 -0800

    Delete setup.py
    Not needed now ig

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='ferium-to-mmm',
-    version='0.1',
-    description='Converts ferium config files to mmm modlist files.',
-    author='Yellowsam'
-)
